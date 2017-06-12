cb785e402e0b32c295d86058fda73dc7f6fdccc7
Kofi Dankwa <Kofi.dankwa1994@hotmail.com>
2017-06-12 01:04:47 +0100

    .

--- option_price_calculator.rb
+++ option_price_calculator.rb
@@ -14,7 +14,7 @@ puts "Welcome to Kofi's option price calculator"
 class Asset
   include Math
 
-attr_accessor :initial_price, :dividend_yield, :interest_rate
+attr_accessor :initial_price, :dividend_yield, :interest_rate, :volatility
 
 # interest_rate, volatility and dividend_yield - percentages i.e 1% = 1, initial_price is a real number
 def initialize(initial_price, dividend_yield, interest_rate, volatility)
@@ -26,25 +26,30 @@ end
 
 # returns the no-arbitrage spot price for the underlying asset s(t) with initial price s(0) // this is a model independent result, so the underlying asset could follow a levy-process or Geometric brownian motion
 def spot(time)
-  @initial_price*Math.exp((@interest_rate - @dividend_yield)*time)
+  @initial_price*Math.exp((@interest_rate - @dividend_yield)*time / 100 )
 end
 
 
 # Here we assume the asset price s(t) follows a Geometric Brownian motion. This is reflected in the following function
 def call_price(strike, time)
   norm = Rubystats::NormalDistribution.new(0,1)
-  d1 = Math.log(spot(time) / strike) + 0.5 * @volatility * Math.sqrt(time)
-  d2 = d1 - @volatility * Math.sqrt(time)
-  Math.exp(-@interest_rate * time)*(spot(time) * norm.cdf(d1) - strike * norm.cdf(d2))
+  d1 = Math.log(spot(time) / strike) + 0.5 * (@volatility / 100 )* Math.sqrt(time)
+  d2 = d1 - ( @volatility / 100 ) * Math.sqrt(time)
+  Math.exp(- (@interest_rate * time / 100))*(spot(time) * norm.cdf(d1) - strike * norm.cdf(d2))
 end
 
-#we calculate put_price using the call-put parity formula
+
 def put_price(strike, time)
-  call_price(strike, time) - Math.exp(-@interest_rate * time) * (spot(time) - strike)
+  norm = Rubystats::NormalDistribution.new(0,1)
+  d1 = Math.log(spot(time) / strike) + 0.5 * (@volatility / 100) * Math.sqrt(time)
+  d2 = d1 - (@volatility / 100) * Math.sqrt(time)
+  Math.exp( -(interest_rate * time) / 100  ) * ( - spot(time) * norm.cdf(-d1) + strike * norm.cdf(-d2))
 end
 
 
 end
 
-p = Asset.new(10, 2, 3, 2)
-puts p.call_price(50, 3)
+
+p = Asset.new(40, 0, 1.5, 27)
+
+puts p.put_price(35, 0.5)
