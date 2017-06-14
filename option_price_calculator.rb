b62dec7a5804ee2b791fe82b6c8b4c5ba57becd7
Kofi Dankwa <Kofi.dankwa1994@hotmail.com>
2017-06-14 23:07:19 +0100

    refactored option_price function and added vega and rho function

--- option_price_calculator.rb
+++ option_price_calculator.rb
@@ -28,29 +28,30 @@ def spot(time)
   @initial_price * Math.exp(@interest_rate * time )
 end
 
-
 # Here we assume the asset price s(t) follows a Geometric Brownian motion. This is reflected in the following function
-def call_price(strike, time)
+def option_price(strike, time)
   norm = Rubystats::NormalDistribution.new(0,1)
   d1 = Math.log(spot(time) / strike) + 0.5 * (@volatility)* Math.sqrt(time)
   d2 = d1 -  @volatility * Math.sqrt(time)
-  Math.exp( - @interest_rate * time ) * ( spot(time) * norm.cdf(d1) - strike * norm.cdf(d2))
+  puts "Do you want the price for a put or call option?"
+  response = gets.chomp.downcase
+  if response == "call"
+    Math.exp( - @interest_rate * time ) * ( spot(time) * norm.cdf(d1) - strike * norm.cdf(d2))
+  elsif response == "put"
+    Math.exp( - @interest_rate * time ) * ( - spot(time) * norm.cdf(-d1) + strike * norm.cdf(-d2))
+  else
+    puts "Sorry I didn't get that. Please try again."
+    option_price(strike, time)
+  end
 end
 
-
-def put_price(strike, time)
-  norm = Rubystats::NormalDistribution.new(0,1)
-  d1 = Math.log(spot(time) / strike) + 0.5 * (@volatility) * Math.sqrt(time)
-  d2 = d1 - @volatility * Math.sqrt(time)
-  Math.exp( - @interest_rate * time ) * ( - spot(time) * norm.cdf(-d1) + strike * norm.cdf(-d2))
-end
-
+# delta is the rate of change of option price with respect to spot
 def delta(strike, time)
   norm = Rubystats::NormalDistribution.new(0,1)
   d1 = Math.log(spot(time) / strike) + 0.5 * (@volatility)* Math.sqrt(time)
   puts "Do you want the delta for a put or call option?"
   response = gets.chomp.downcase
-  if response === "call"
+  if response == "call"
     norm.cdf(d1)
   elsif response == "put"
     norm.cdf(d1) - 1
@@ -60,8 +61,42 @@ def delta(strike, time)
   end
 end
 
+# vega is the rate of change of option price with respect to volatility
+def vega(strike, time)
+  norm = Rubystats::NormalDistribution.new(0,1)
+  d1 = Math.log(spot(time) / strike) + 0.5 * (@volatility)* Math.sqrt(time)
+  puts "Do you want the vega for a put or call option?"
+  response = gets.chomp.downcase
+  if response == "call"
+    norm.pdf(d1) * spot(time) * Math.sqrt(time)
+  elsif response == "put"
+    norm.pdf(d1) * spot(time) * Math.sqrt(time)
+  else
+    puts "Sorry I didn't get that. Please try again."
+    vega(strike, time)
+  end
+end
+
+# rho is the rate of change of option price with respect to interest rate
+def rho(strike, time)
+  norm = Rubystats::NormalDistribution.new(0,1)
+  d1 = Math.log(spot(time) / strike) + 0.5 * (@volatility)* Math.sqrt(time)
+  d2 = d1 - @volatility * Math.sqrt(time)
+  puts "Do you want the rho for a put or call option?"
+  response = gets.chomp.downcase
+  if response == "call"
+    #
+  elsif response == "put"
+    #
+  else
+    puts "Sorry I didn't get that. Please try again. "
+    rho(strike, time)
+  end
+end
+
 end
 
 p = Asset.new(20, 0.50, 20.00)
 
 puts p.delta(40, 1)
+puts p.vega(40, 1)
